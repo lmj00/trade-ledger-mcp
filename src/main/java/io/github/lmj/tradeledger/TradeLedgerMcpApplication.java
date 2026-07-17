79ed038219838478bc0207d8d5fbfa5cfca4b821
lmj <e.minjin00@gmail.com>
2026-07-18 07:15:23 +0900

    chore: bootstrap Java 21 MCP server

---
+++ src/main/java/io/github/lmj/tradeledger/TradeLedgerMcpApplication.java
@@ -0,0 +1,13 @@
+package io.github.lmj.tradeledger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeLedgerMcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradeLedgerMcpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/lmj/tradeledger/TradeLedgerMcpApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.lmj.tradeledger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeLedgerMcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
